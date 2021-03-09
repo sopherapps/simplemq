202144cd230e9fd6a20f8af6a268d7d6afa0c346
Martin Ahindura <martinahindura@gmail.com>
2021-03-09 22:10:10 +0300

    Add subscribers model

--- src/db/messages.js
+++ src/db/messages.js
@@ -12,14 +12,19 @@ class Messages extends Model {
     });
     this.schema = {
       type: "object",
+      description: "The message saved in the queue",
       properties: {
         id: {
+          description:
+            "The internally automatically generated id for the message",
           type: "string",
         },
         topic: {
+          description: "The topic to which the message is posted",
           type: "string",
         },
         data: {
+          description: "The actual payload",
           type: "object",
           patternProperties: {
             "^.*$": {},
---
+++ src/db/subscribers.js
@@ -0,0 +1,44 @@
+/**
+ * Model for the subscribers
+ */
+
+const Model = require("./base/model");
+
+class Subscribers extends Model {
+  constructor() {
+    super("subscribers", "clientId");
+    this.schema = {
+      type: "object",
+      description:
+        "The subscribers that have reqistered with this message queue",
+      properties: {
+        clientId: {
+          description:
+            "The client generated unique id used to identify each client on reconnection",
+          type: "string",
+        },
+        messageIds: {
+          description:
+            "The ids of yet-to-be-consumed messages for a given client saved as an object of string: integer",
+          type: "object",
+          patternProperties: {
+            "^.*$": { type: "integer" },
+          },
+          additionalProperties: false,
+        },
+        messageIdsInOrder: {
+          description:
+            "An array of the ids of yet-to-be-consumed messages for a given client, in order of creation",
+          type: "array",
+          items: {
+            type: "string",
+          },
+        },
+      },
+    };
+  }
+}
+
+module.exports = {
+  Subscribers,
+};
